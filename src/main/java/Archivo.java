725527bf07884aeaac7732226b1511e37435e0b1
brockar <martinnguzman.mg@gmail.com>
2023-04-24 16:57:31 -0300

    asd

--- src/main/java/Archivo.java
+++ src/main/java/Archivo.java
@@ -16,7 +16,7 @@ public class Archivo {
     public static void main(String[] args) {
         //Lectura de configuracion
 
-        HashMap<String, String> configuracion = new HashMap<String, String>();
+        HashMap<String, String> configuracion = new HashMap<>();
         try {
             // Abre el archivo de configuración
             BufferedReader conf = new BufferedReader(new FileReader("src/main/java/config.txt"));
@@ -90,19 +90,18 @@ public class Archivo {
 
 
         // Lectura PRONOSTICOS -------------------
+        ArrayList<Pronostico> aPro = new ArrayList<>();
         Pronostico pro;
-        List<String> aPro = new ArrayList<String>();
+        String[] datos0;
 
-        try {
-            Class.forName("com.mysql.cj.jdbc.Driver");
-            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/pronosticofinal", "root", "");
-            Statement stmt = con.createStatement();
-            ResultSet rs = stmt.executeQuery("SELECT * FROM tabla1");
-            while (rs.next()) {
+        try (Scanner scFile = new Scanner(new File("src/main/java/pronostico.csv"))) {
+            while (scFile.hasNextLine()) {
+                datos0 = scFile.nextLine().split(";");
                 pro = new Pronostico();
 
-                pro.setNumPartido(rs.getInt("NumPartido"));
+                pro.setNumPartido(Integer.parseInt(datos0[0]));
 
+                // Mira que partido es y si existe.
                 boolean existe = false;
                 for (Partido pa : aP) {
                     if (pa.getNumPartido() == pro.getNumPartido()) {
@@ -110,39 +109,36 @@ public class Archivo {
                         break;
                     }
                 }
+
                 if (existe) {
                     Equipo e = new Equipo();
-                    e.setNombre(rs.getString("Equipo1"));
+                    e.setNombre(datos0[1]);
                     Equipo e1 = new Equipo();
-                    e1.setNombre(rs.getString("Equipo2"));
-
-
-                    if (rs.getInt("GanaEquipo1") == 1) {
+                    e1.setNombre(datos0[5]);
+                    //Se fija por quien aposto el del pronostico
+                    //Si esta marcado la columna 1 es que apuesta por el equipo1 (e),
+                    //Si esta marcada la columna 3 es que apuesta por el equipo2 (e1),
+                    //Si esta marcada la columna del medio es empate, por lo tanto no se asigna equipo.
+                    if (!datos0[2].isEmpty() && Integer.parseInt(datos0[2]) == 1) {
                         pro.setEquipo(e);
-                    } else if (rs.getInt("GanaEquipo2") == 1) {
+                    } else if (!datos0[4].isEmpty() && Integer.parseInt(datos0[4]) == 1) {
                         pro.setEquipo(e1);
-
                     }
-                    pro.setNombre("Nombre");
-                    pro.setRonda(Integer.parseInt("Ronda"));
-                    pro.setFase(Integer.parseInt("Fase"));
 
+                    pro.setNombre(datos0[6]);
+                    pro.setRonda(Integer.parseInt(datos0[7]));
+                    pro.setFase(Integer.parseInt(datos0[8]));
 
                     //busca el ResultadoEnum del equipo que aposto y lo agrega
                     pro.setResultado(aP.get(pro.getNumPartido()).resultadoPart(pro.getEquipo()));
                     //Agrega los puntos correspondientes a los que tenia.
                     pro.setPuntos(pro.fpuntos());
-                    aPro.add(String.valueOf(pro));
-
+                    aPro.add(pro);
                 }
-
-
             }
-
-        } catch (SQLException | ClassNotFoundException e) {
-            throw new RuntimeException(e);
+        } catch (Exception error1) {
+            error1.printStackTrace();
         }
-
 
 
 //      Recuento PUNTOS  --------------------------------------------
@@ -216,21 +212,23 @@ public class Archivo {
 //        }
 
         //Puntos por partido viejo
-//        //M=2 P=3
+//      M=4
+//      P=8 2 fase, 3 ronda
         int puntosPersona = 0;
         String nombrePersona = "";
 
-        int[] partxronda = new int[10];
-        int iter=0;
         int multip = Integer.parseInt(configuracion.get("PuntosPart"));
+        int ronda=0;
+        int fase=0;
+        int cuentarondas=0;
+        int rondasbien=0;
+        int puntosar=0;
+        int cuentafase=0;
+        int puntosafase=0;
+        int fasesbien=0;
 
-<<<<<<< HEAD
         int masRonda=Integer.parseInt(configuracion.get("PuntosRonda"));
-        int masFase=Integer.parseInt(configuracion.get("PuntosFase"));
-=======
-//        HashMap<String, int[]> rondasg = new HashMap<String, int[]>();
-//        int[] a = new int [10];
->>>>>>> parent of 5e902c5 (cuento rondas y falta fases)
+        int masFase=Integer.parseInt(configuracion.get("PuntosFase"))
 
         //#Tendria que guardarlos en un arraylist con todos los partidos que acertaron y ver si son de la misma ronda y fase para darle bien los puntos extras
         //ya que las fases pueden ser de dos rondas no contiguas
@@ -240,52 +238,88 @@ public class Archivo {
             if (i == 0) {
                 nombrePersona = pronostico.getNombre();
                 puntosPersona = pronostico.getPuntos() * multip ;
-                partxronda[iter]=1;
+
+                ronda=pronostico.getRonda();
+                fase=pronostico.getFase();
+                cuentarondas++;
+                cuentafase++;
             }
 //            resto de entradas excepto la ultima
-            else if (nombrePersona.equals(pronostico.getNombre()) && i != aPro.size() - 1) {
+            else if (nombrePersona.equals(pronostico.getNombre()) && i!=aPro.size()-1) {
+
+                if(ronda!=pronostico.getRonda()){
+                    if(cuentarondas==(puntosPersona/multip)-puntosar) {
+                        puntosPersona=puntosPersona + masRonda;
+                        rondasbien++;
+                    }
+                    ronda=pronostico.getRonda();
+                    puntosar=puntosPersona/multip;
+                    cuentarondas=0;
+                }
+                cuentarondas++;
+
+                if(fase!=pronostico.getFase()){
+                    if(cuentafase==(puntosPersona/multip-puntosafase)){
+                        puntosPersona=puntosPersona+ masFase;
+                    }
+                    fase=pronostico.getFase();
+                    puntosafase=puntosPersona/multip;
+                    cuentafase=0;
+                }
+                cuentafase++;
+
                 puntosPersona = puntosPersona + (pronostico.getPuntos() * multip );
-                partxronda[iter]++;
             }
+
 //            ultima entrada
             else if (i == aPro.size() - 1) {
+                if(ronda!=pronostico.getRonda()){
+                    ronda=pronostico.getRonda();
+                    cuentarondas=1;
+                    puntosar=puntosPersona/multip;
+                }
+
+                if(fase!=pronostico.getFase()){
+                    fase=pronostico.getFase();
+                    cuentafase=1;
+                    puntosafase=puntosPersona/multip;
+                }
+
                 puntosPersona = puntosPersona + (pronostico.getPuntos() * multip);
-
-                partxronda[iter]++;
-
-                if(partxronda[iter]==(puntosPersona/multip)){
-                    puntosPersona= puntosPersona + Integer.parseInt(configuracion.get("PuntosRonda"));
-//                  para saber las rondas que tienen ganadas
-//                    if(rondasg.get(nombrePersona)!=null) {
-//                        a = rondasg.get(nombrePersona);
-//                    }
-//                    a[pronostico.getRonda()-1]++;
-//                    rondasg.put(nombrePersona,a);
+                if(cuentarondas==(puntosPersona/multip)-puntosar) {
+                    puntosPersona = puntosPersona + masRonda;
                 }
 
+
+                if(cuentafase==(puntosPersona/multip-puntosafase)){
+                    puntosPersona=puntosPersona+ masFase;
+                }
                 System.out.println(nombrePersona + " obtuvo " + puntosPersona + " puntos.");
                 }
+
+//            Cambio de nombre
             else {
-                if(partxronda[iter]==(puntosPersona/multip)){
-                    puntosPersona= puntosPersona + Integer.parseInt(configuracion.get("PuntosRonda"));
-//                  para saber las rondas que tienen ganadas
-//                    if(rondasg.get(nombrePersona)!=null) {
-//                        a = rondasg.get(nombrePersona);
-//                    }
-//                    a[pronostico.getRonda()-1]++;
-//                    rondasg.put(nombrePersona,a);
+                if(cuentarondas==(puntosPersona/multip)-puntosar) {
+                    puntosPersona=puntosPersona + masRonda;
                 }
+                ronda=pronostico.getRonda();
+                cuentarondas=1;
+
+                if(cuentafase==(puntosPersona/multip)-puntosafase){
+                    puntosPersona=puntosPersona+ masFase;
+                }
+                fase=pronostico.getFase();
+                cuentafase=1;
+
                 System.out.println(nombrePersona + " obtuvo " + puntosPersona + " puntos.");
-                iter++;
-                partxronda[iter]++;
                 nombrePersona= pronostico.getNombre();
                 puntosPersona= pronostico.getPuntos() * multip;
+
+                puntosar=0;
+                puntosafase=0;
             }
 
 
-//            int[] fases = new int[3];
-//            fases[0]=1;
-//
 //            for (String clave : rondasg.keySet()) {
 //                boolean fase = true;
 //                int[] valores = rondasg.get(clave);
@@ -326,8 +360,8 @@ public class Archivo {
 //        }
 
 
-    }}
-
+    }
+}
 /*
 Entrega 3
 En esta entrega se deben poder leer los pronósticos desde una base de datos MySQL.
